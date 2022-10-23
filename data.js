01651c871f03fbfb871e9e37b5d527c4c62878a2
itsrm301 <92841332+itsrm301@users.noreply.github.com>
2022-10-23 18:20:03 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,44 @@
+const Ixeria = [
+    {
+        id:"1",
+        title:"Extencee Plus",
+        image:"https://th.bing.com/th/id/OIP.6HOulF05kXqi90G3kq0bgwEoDS?w=227&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        description:"It is A protein suplliment used for maintaining the level of protein in human body. the uniquness of this product is it can be used for both human and animals as well"
+
+    },
+    {
+        id:"2",
+        title:"Extencee Plus",
+        image:"https://th.bing.com/th/id/OIP.6HOulF05kXqi90G3kq0bgwEoDS?w=227&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        description:"It is A protein suplliment used for maintaining the level of protein in human body. the uniquness of this product is it can be used for both human and animals as well"
+
+    },
+    {
+        id:"3",
+        title:"Extencee Plus",
+        image:"https://th.bing.com/th/id/OIP.6HOulF05kXqi90G3kq0bgwEoDS?w=227&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        description:"It is A protein suplliment used for maintaining the level of protein in human body. the uniquness of this product is it can be used for both human and animals as well"
+
+    },
+    {
+        id:"4",
+        title:"Extencee Plus",
+        image:"https://th.bing.com/th/id/OIP.6HOulF05kXqi90G3kq0bgwEoDS?w=227&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        description:"It is A protein suplliment used for maintaining the level of protein in human body. the uniquness of this product is it can be used for both human and animals as well"
+
+    },
+    {
+        id:"5",
+        title:"Extencee Plus",
+        image:"https://th.bing.com/th/id/OIP.6HOulF05kXqi90G3kq0bgwEoDS?w=227&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        description:"It is A protein suplliment used for maintaining the level of protein in human body. the uniquness of this product is it can be used for both human and animals as well"
+
+    },
+    {
+        id:"6",
+        title:"Extencee Plus",
+        image:"https://th.bing.com/th/id/OIP.6HOulF05kXqi90G3kq0bgwEoDS?w=227&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        description:"It is A protein suplliment used for maintaining the level of protein in human body. the uniquness of this product is it can be used for both human and animals as well"
+
+    },
+]
